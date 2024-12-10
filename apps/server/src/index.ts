72fc5e2a109e092cbd053c9f3fd802b13d81314c
dalton <dakribe94@gmail.com>
2024-12-10 17:31:54 -0600

    configure drizzle

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
-import { greeting, name } from "@kend/core";
 
 const app = new Hono();
 
-const message = greeting(name);
-
 app.get("/", (c) => {
-  return c.json({ message });
+	return c.json({ message: "Hello Kend" });
 });
 
 export default app;
--- packages/core/index.ts
+++
@@ -1,5 +0,0 @@
-export function greeting(name: string) {
-  return `Hello ${name}!`;
-}
-
-export const name = "dakribe";
