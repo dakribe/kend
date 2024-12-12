d372299126916be6a9f737b97e6dbadcbf6ecb90
dalton <dakribe94@gmail.com>
2024-12-12 13:55:54 -0600

    add tailwind

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Index from "./pages";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Index />} />
+				<Route path="/signin" element={<Login />} />
+				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
