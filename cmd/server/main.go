0bf267e9be923c8df3ae51e5d80906430ee6fe5e
dalton <dakribe94@gmail.com>
2025-02-17 15:45:28 -0600

    postgres setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/dakribe/kend/internal/postgres"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db := postgres.NewPostgres(os.Getenv("DATABASE_URL"))
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 
