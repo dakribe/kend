99699a3575926f6ba5d8c03921a70845aa80faa4
dalton <dakribe94@gmail.com>
2025-02-21 21:18:45 -0600

    shadcn config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
 	return (
 		<div>
 			<p className="text-3xl font-bold text-red-600">Hello world</p>
+			<Button>Button</Button>
 		</div>
 	);
 }
