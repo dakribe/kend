8bd32a5915cfdda97a8df18180768a530e0e6316
dalton <dakribe94@gmail.com>
2025-04-07 12:28:19 -0500

    shadcn ui init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+	return (
+		<div>
+			<p>Index</p>
+			<Button>Button</Button>
+		</div>
+	);
 }
