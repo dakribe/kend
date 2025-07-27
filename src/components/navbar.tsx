598520533d2d9b2a7f01019aa2d81e50cbe81e2c
dakribe <dakribe94@gmail.com>
2025-07-27 10:16:59 -0500

    active link styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,47 @@
 import { Link } from "@tanstack/react-router";
-import { Bookmark, Home, List, Plus, Settings } from "lucide-react";
+import { never } from "better-auth";
+import { Bookmark, Home, List, LucideIcon, Plus, Settings } from "lucide-react";
 
 interface Props {
 	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+type NavItem = {
+	icon: LucideIcon;
+} & ({ href: string; onClick?: never } | { onClick: () => void; href?: never });
+
 export function Navbar({ setOpen }: Props) {
+	const navItems: NavItem[] = [
+		{ icon: Home, href: "/dashboard" },
+		{ icon: List, href: "/applications" },
+		{ icon: Plus, onClick: () => setOpen(true) },
+	];
+
 	return (
-		<div className="fixed rounded-3xl bottom-10 left-1/2 transform -translate-x-1/2 border h-12 flex items-center p-2">
-			<Link to="/dashboard" className="hover:cursor-default">
-				<div className="hover:bg-accent rounded-full p-2">
-					<Home className="size-4" />
-				</div>
-			</Link>
-			<Link to="/applications" className="hover:cursor-default">
-				<div className="hover:bg-accent rounded-full p-2">
-					<List className="size-4" />
-				</div>
-			</Link>
-			<Link to="/applications" className="hover:cursor-default">
-				<div className="hover:bg-accent rounded-full p-2">
-					<Bookmark className="size-4" />
-				</div>
-			</Link>
-			<Link to="/applications" className="hover:cursor-default">
-				<div className="hover:bg-accent rounded-full p-2">
-					<Settings className="size-4" />
-				</div>
-			</Link>
-			<div
-				onClick={() => setOpen(true)}
-				className="hover:bg-accent rounded-full p-2"
-			>
-				<Plus className="size-4" />
-			</div>
+		<div className="fixed rounded-3xl bottom-10 left-1/2 transform -translate-x-1/2 border h-12 flex items-center p-2 gap-1">
+			{navItems.map((item, index) =>
+				item.href ? (
+					<Link
+						key={index}
+						to={item.href}
+						activeProps={{
+							className: "bg-accent rounded-full",
+						}}
+					>
+						<div className="hover:bg-accent rounded-full p-2 cursor-default">
+							<item.icon className="size-4" />
+						</div>
+					</Link>
+				) : (
+					<div
+						key={index}
+						onClick={item.onClick}
+						className="hover:bg-accent rounded-full p-2"
+					>
+						<item.icon className="size-4" />
+					</div>
+				),
+			)}
 		</div>
 	);
 }
