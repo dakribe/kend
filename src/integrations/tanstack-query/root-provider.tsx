1b6e72eefce62972022b689aa301db2ebcc2b370
dakribe <dakribe94@gmail.com>
2026-03-07 21:07:19 -0600

    enable cookie cache

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -12,7 +12,13 @@ export function getContext() {
     return context;
   }
 
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   context = {
     queryClient,
